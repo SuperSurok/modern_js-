95e42fac55ef4e669039470acb256111d76ac1d3
Sergey Lebedev <oddyone@mail.ru>
2018-05-14 00:19:11 +0300

    Template literals

--- app.js
+++ app.js
@@ -1,63 +1,37 @@
-const firstName = 'Sergey';
-const lastName = 'Lebedev';
+const name = 'Sergey';
 const age = 36;
-const str = 'Hello there, my name is Sergey';
-const tags = 'web design, web development, programming';
+const job = 'Web developer';
+const city = 'Saint-P';
+let html;
 
-let val;
+// Without template strings (es5)
+html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + '</li><li>Job: ' + job + '</li><li>City: ' + city + '</li></ul>';
 
-val = firstName + lastName;
-
-// Conacatenation
-val = firstName + ' ' + lastName;
-
-// Append
-val = 'Pasha ';
-val += 'Kashin';
-val = 'Hello, my name is ' + firstName + ' and I am ' + age;
-
-// Escaping
-val = "That's awesome, I can't wait";
-
-// length
-val = firstName.length;
-
-// concat()
-val = firstName.concat(' ', lastName);
-
-// Change case
-val = firstName.toUpperCase();
-val = lastName.toLowerCase();
-
-// Index char
-val = firstName[2];
-
-// indexOf()
-val = firstName.indexOf('S');
-val = firstName.lastIndexOf('r');
-
-// charAt()
-val = firstName.charAt('5');
-
-// Get last char
-val = firstName.charAt(firstName.length - 1);
-
-// substring
-val = firstName.substring(0, 4);
-
-// slice()
-val = firstName.slice(0, 4);
-val = firstName.slice(-3);
-
-// split()
-val = str.split(' ');
-val = tags.split(',');
-
-// replace()
-val = str.replace('Sergey', 'Pasha');
-
-// includes()
-val = str.includes('Hello');
+html = '<ul>' +
+    '<li>Name: ' + name + '</li>' +
+    '<li>Age: ' + age + '</li>' +
+    '<li>Job: ' + job + '</li>' +
+    '<li>City: ' + city + '</li>';
 
 
-console.log(val);
+function hello() {
+    return 'hello';
+}
+
+// With template strings (es6)
+html = `
+  <ul>
+    <li>Name: ${name}</li>
+    <li>Age: ${age}</li>
+    <li>Job: ${job}</li>
+    <li>City: ${city}</li>
+    <li>${2 + 2}</li>
+    <li>${hello()}</li>
+    <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+  </ul>
+`;
+
+
+document.body.innerHTML = html;
+
+console.log(html);
